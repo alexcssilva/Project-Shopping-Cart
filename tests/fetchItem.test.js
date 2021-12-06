562ed421d0464a60bec433da9a59663e54f294fa
Alex Silva <alexcssilva@gmail.com>
2021-12-06 18:52:12 -0300

    Desafio 04 concluido - fix test

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -1,3 +1,4 @@
+require('../mocks/fetchSimulator');
 const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
@@ -5,19 +6,19 @@ describe('2 - Teste a função fecthItem', () => {
   it ('Teste se fetchItem é uma função', () => {
     expect(typeof fetchItem).toBe('function');
   });
-  it ('Execute a função fetchItem com o argumento do item "MLB1615760527" e teste se fetch foi chamada',async () => {
-    fetchItem('MLB1615760527')
+  it ('Execute a função fetchItem com o argumento do item "MLB1615760527" e teste se fetch foi chamada', async () => {
+    await fetchItem('MLB1615760527');
     expect(fetch).toHaveBeenCalled();
   });
   it ('Teste se, ao chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint "https://api.mercadolibre.com/items/MLB1615760527"', async () => {
-    fetchItem('MLB1615760527')
+    await fetchItem('MLB1615760527')
     expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
   });
-  it ('Teste se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto item que já está importado no arquivo.',async () => {
+  it ('Teste se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto item que já está importado no arquivo.', async () => {
     const objFetchItem = await fetchItem('MLB1615760527');
     expect(objFetchItem).toBe(item);
   });
-  it ("Teste se, ao chamar a função fetchItem sem argumento, retorna um erro com a mensagem: You must provide an url. Dica: Lembre-se de usar o new Error('mensagem esperada aqui') para comparar com o objeto retornado da API.",async () => {
+  it ("Teste se, ao chamar a função fetchItem sem argumento, retorna um erro com a mensagem: You must provide an url. Dica: Lembre-se de usar o new Error('mensagem esperada aqui') para comparar com o objeto retornado da API.", async () => {
     const objFetchItem = await fetchItem();
     const error = new Error('You must provide an url');
     expect(objFetchItem).toEqual(error);
